8eb7c5cae849b510926b861b719034a1248a2031
liujia <lj130138@qq.com>
2025-11-11 17:47:55 +0800

    添加chart

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,14 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 
-
 const routes: RouteRecordRaw[] = [
+  {
+    path:'/',
+    redirect:'/dashboard',
+  },
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/dashboard',
+        name: 'dashboard',
+        meta: {
+          title: '系统首页',
+          noAuth: true,
+        },
+        component: () => import(/* webpackChunkName: "dashboard" */ '../views/dashboard.vue'),
+      },
+    ],
   },
-
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/chart/echarts.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <el-row>
+
+        </el-row>
+    </div>
+</template>
+<style scoped>
+.schart {
+    width: 100%;
+    height: 400px;
+}
+
+.content-title {
+    font-weight: 400;
+    font-size: 22px;
+    color: #1f2f3d;
+}
+</style>
