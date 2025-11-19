e9a96e427d64f15776b6c095a0bcf9881e7440f2
liujia <lj130138@qq.com>
2025-11-19 17:09:32 +0800

    部分修改

--- src/components/table-search.vue
+++ src/components/table-search.vue
@@ -2,7 +2,7 @@
 import type { FormInstance } from 'element-plus'
 import { Search, Refresh } from '@element-plus/icons-vue'
 import { type PropType, ref, type Prop } from 'vue'
-import type { FromOptionList } from '@/types/form-option'
+import type { FormOptionList } from '@/types/form-option'
 
 const props = defineProps({
     query: {
@@ -10,32 +10,41 @@ const props = defineProps({
         required: true
     },
     options: {
-        type: Array as PropType<Array<FromOptionList>>,
+        type: Array as PropType<Array<FormOptionList>>,
         required: true
     },
-    Search: {
+    search: {
         type: Function,
         default: () => { }
     }
 })
 
 const searchRef = ref<FormInstance>()
-const resetForm = (formEl:FormInstance | undefined)=>{
-    if(!formEl) return 
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
     formEl.resetFields()
-    props.Search()
+    props.search()
 }
 </script>
 
 <template>
-<div class="search-container">
-    <el-form ref="searchRef" :model="query" :inline="true">
-        <el-form-item>
+    <div class="search-container">
+        <el-form ref="searchRef" :model="query" :inline="true">
+            <el-form-item :label="item.label" v-for="item in options" :prop="item.prop">
 
-        </el-form-item>
-
-    </el-form>
-</div>
+                <el-input v-if="item.type === 'input'" v-model="query[item.prop]" :disabled="item.disabled"
+                    :placehold="item.placeholder" clearable></el-input>
+                <el-select v-else-if="item.type === 'select'" v-model="query[item.prop]" :display="item.disabled"
+                    :placeholder="item.placeholder" clearable></el-select>
+                <el-date-picker v-else-if="item.type === 'date'" type="date" v-model="query[item.prop]"
+                    :value-format="item.format"></el-date-picker>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="search" :icon="Search">搜索</el-button>
+                <el-button :icon="Refresh" @click="resetForm(searchRef)">重置</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 
 </template>
 
--- src/views/goods/goodList.vue
+++ src/views/goods/goodList.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
 
+import type { FormOptionList, FormOption } from '@/types/form-option'
+
+import { Search } from '@element-plus/icons-vue'
+import { labelInner } from 'echarts/types/src/label/labelStyle.js'
+
+import tableSearch from '@/components/table-search.vue'
+import tableCustom from '@/components/table-custom.vue'
+const query = reactive({
+    name: ''
+})
+
+const searchOpt = ref<FormOptionList[]>([
+    { type:'input',label:'',prop:'name'}
+])
+const handleSearch = () => {}
 </script>
 
 <template>
-
-
+    <div>
+        <tableSearch :query="query" :options="searchOpt" :search="handleSearch" />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
